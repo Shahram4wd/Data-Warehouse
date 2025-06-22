2208fe444d251d0f1500eca04d428d0286198062
Shahram Haj mohammadi <Shahram4wd@yahoo.com>
2025-06-22 00:07:36 -0700

    feat: Remove deprecated files related to Arrivy integration and authentication tests

--- check_groups.py
+++
@@ -1,6 +0,0 @@
-from ingestion.models import Arrivy_Group
-
-print(f'Total groups: {Arrivy_Group.objects.count()}')
-print('Sample groups:')
-for group in Arrivy_Group.objects.all()[:5]:
-    print(f'  {group.id}: {group.name} - {group.city}, {group.state}')
