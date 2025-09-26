1bcb99dd6d3ad93f5efbf523b0a72ee9099c6759
Shahram <shahram4wd@yahoo.com>
2025-09-26 12:06:59 -0700

    feat: Remove unused model imports and simplify available model references

--- ingestion/models.py
+++ ingestion/models.py
@@ -1,10 +1,4 @@
 from .models.common import SyncSchedule, SyncHistory
-from .models.genius import (
-    DivisionGroup, Division, UserData, Prospect, ProspectSource,
-    AppointmentType, AppointmentOutcome, Appointment, AppointmentService,
-    Service, Quote, MarketingSourceType, MarketingSource, AppointmentOutcomeType
-)
-from .models.hubspot import Hubspot_Contact, Hubspot_Deal, Hubspot_SyncHistory
 from .models.marketsharp import (
     MarketSharp_Activity, MarketSharp_ActivityReference, MarketSharp_ActivityResult,
     MarketSharp_Address, MarketSharp_Appointment, MarketSharp_AppointmentResult,
@@ -16,24 +10,7 @@ from .models.marketsharp import (
     MarketSharp_ProductType, MarketSharp_Prospect
 )
 
-# Keep all the model references available
+# Keep common models available
 __all__ = [
-    # Schedule models
-    'SyncSchedule', 'SyncHistory',
-    # Common models
-    # Genius models
-    'DivisionGroup', 'Division', 'UserData', 'Prospect', 'ProspectSource',
-    'AppointmentType', 'AppointmentOutcome', 'Appointment', 'AppointmentService',
-    'Service', 'Quote', 'MarketingSourceType', 'MarketingSource', 'AppointmentOutcomeType',
-    # Hubspot models
-    'Hubspot_Contact', 'Hubspot_Deal', 'Hubspot_SyncHistory',
-    # MarketSharp models
-    'MarketSharp_Activity', 'MarketSharp_ActivityReference', 'MarketSharp_ActivityResult',
-    'MarketSharp_Address', 'MarketSharp_Appointment', 'MarketSharp_AppointmentResult',
-    'MarketSharp_Company', 'MarketSharp_Contact', 'MarketSharp_ContactPhone',
-    'MarketSharp_ContactType', 'MarketSharp_CustomField', 'MarketSharp_Customer',
-    'MarketSharp_Employee', 'MarketSharp_Inquiry', 'MarketSharp_InquirySourcePrimary',
-    'MarketSharp_InquirySourceSecondary', 'MarketSharp_InquiryStatus', 'MarketSharp_Job',
-    'MarketSharp_Lead', 'MarketSharp_ProductDetail', 'MarketSharp_ProductInterest',
-    'MarketSharp_ProductType', 'MarketSharp_Prospect'
+    'SyncSchedule', 'SyncHistory'
 ]
